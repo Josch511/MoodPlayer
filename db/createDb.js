5d3a6f0d070eb3be90c59745f03a815ff6384f7d
sebkahr <sebkahr@gmail.com>
2025-11-28 12:46:29 +0100

    added mood and tracks and fixed some things

--- db/createDb.js
+++ db/createDb.js
@@ -17,21 +17,21 @@ console.log('Importing data from CSV files...');
 
 console.log('Database recreated.');
 
-await upload(`
-	drop table if exists albums;
+await db.query(`
+	drop table if exists mood;
 	create table mood (
-	mood_id              integer,
-	mood_name            tekst,
-	description          tekst    
+        mood_id        integer,
+	    mood_name      tekst,
+	    description    tekst    
 	
-	);
+);
 `);
 
 await db.query(`
     drop table if exists user_interaction;
     create table user_interaction (
         interaction_id    integer,
-        track_id      	  integer  references track_id
+        track_id      	  integer  references track_id,
         like			  integer,
         dislike		      integer
 );
@@ -40,10 +40,22 @@ await db.query(`
 await db.query(`
     drop table if exists current_play;
     create table current_play (
-        track_id    integer	 references track_id
+        track_id    integer	 references track_id,
         is_playing  boolean  
     );
 `);
 
-
+await db.query(`
+    drop table if exists tracks;
+    create table tracks (
+        tracks_id        integer primary key,
+        title            tekst,
+        artist           tekst,
+        genre            tekst,
+        bpm              integer,
+        duration         integer,
+        album_cover      tekst,
+        release_year     integer       
+);
+`);
 await db.end();
