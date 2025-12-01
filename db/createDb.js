07b72f64ba52e6f18c4565441fc74bc24d9e3aa4
Jonathan Schmidt <josch511@icloud.com>
2025-12-01 13:44:12 +0100

    DB changed and fixed

--- db/createDb.js
+++ db/createDb.js
@@ -6,56 +6,61 @@ console.log('Recreating database...');
 const db = await connect();
 
 console.log('Dropping tables...');
-await db.query('drop table if exists user_interaction');
-await db.query('drop table if exists current_play');
-await db.query('drop table if exists tracks');
-await db.query('drop table if exists playlists');
+await db.query('DROP TABLE IF EXISTS playlists');
+await db.query('DROP TABLE IF EXISTS tracks');
 console.log('All tables dropped.');
 
 console.log('Recreating tables...');
 
-// Erst tracks erstellen (wird von anderen referenziert)
 await db.query(`
-    create table tracks (
-        track_id         integer primary key,
-        title            text,
-        artist           text,
-        genre            text,
-        bpm              integer,
-        duration         integer,
-        album_cover      text,
-        release_year     integer       
-    );
+    CREATE TABLE tracks (
+        number           INTEGER,
+        track_id         TEXT,
+        artists          TEXT,
+        album_name       TEXT,
+        track_name       TEXT,
+        popularity       INTEGER,
+        duration_ms      INTEGER,
+        explicit         BOOL,
+        danceability     DOUBLE PRECISION,
+        energy           DOUBLE PRECISION,
+        key              INTEGER,
+        loudness         DOUBLE PRECISION,
+        mode             INTEGER,
+        speechiness      DOUBLE PRECISION,
+        acousticness     DOUBLE PRECISION,
+        instrumentalness DOUBLE PRECISION,
+        liveness         DOUBLE PRECISION,
+        valence          DOUBLE PRECISION,
+        tempo            DOUBLE PRECISION,
+        time_signature   INTEGER,
+        track_genre      TEXT
+    )
 `);
 
 await db.query(`
-    create table playlists (
-        playlist_id        integer primary key,
-        playlist_name      text    
-    );
-`);
-
-await db.query(`
-    create table user_interaction (
-        interaction_id    integer primary key,
-        track_id          integer references tracks(track_id),
-        like_count        integer,
-        dislike_count     integer
-    );
-`);
-
-await db.query(`
-    create table current_play (
-        track_id       integer references tracks(track_id),
-        is_playing     boolean  
-    );
+    CREATE TABLE playlists (
+        playlist_id   SERIAL,
+        playlist_name TEXT
+    )
 `);
 
 console.log('Tables recreated.');
 
-await upload(db, 'db/tracks.csv',
-  'copy tracks (track_id, artists, track_name, popularity, duration_ms, explicit, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, time_signature, track_genre) from stdin with csv header');
+await upload(db, 'db/tracks.csv', `
+    COPY tracks (
+        number, track_id, artists, album_name, track_name, popularity, duration_ms, explicit,
+        danceability, energy, key, loudness, mode,
+        speechiness, acousticness, instrumentalness, liveness,
+        valence, tempo, time_signature, track_genre
+    ) FROM STDIN WITH CSV HEADER
+`);
 
-await upload(db, 'db/userinteraction.csv',
-    'copy userinteraction (interaction_id, track_id, like, dislike) from stdin with csv header');
+await upload(db, 'db/playlists.csv', `
+    COPY playlists (
+        playlist_id, playlist_name
+    ) FROM STDIN WITH CSV HEADER
+`);
+
 await db.end();
+console.log('CSV data uploaded and DB connection closed.');
