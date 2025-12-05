374a8efa5ba30b9bdc09e2d16ba995727ea653a0
Jonathan Schmidt <josch511@icloud.com>
2025-12-05 10:50:14 +0100

    deleted file py

--- db/filter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("tracks.csv")
-
-# Nur Zeilen behalten, deren Wert >= 50 ist
-df = df[df["popularity"] >= 15]
-
-# Neue Datei speichern
-df.to_csv("filtered.csv", index=False)
